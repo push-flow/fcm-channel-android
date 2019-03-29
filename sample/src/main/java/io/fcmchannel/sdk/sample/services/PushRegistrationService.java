89f53b85fccd16813d39c4c3543ad46efd4afba7
Erick Jones <erick.jones@ilhasoft.com.br>
2019-03-29 17:08:11 -0300

    Fix contact package reference

--- sample/src/main/java/io/fcmchannel/sdk/sample/services/PushRegistrationService.java
+++ sample/src/main/java/io/fcmchannel/sdk/sample/services/PushRegistrationService.java
@@ -3,7 +3,7 @@ package io.fcmchannel.sdk.sample.services;
 import android.util.Log;
 
 import io.fcmchannel.sdk.FcmClient;
-import io.fcmchannel.sdk.core.models.v2.Contact;
+import io.fcmchannel.sdk.core.models.Contact;
 import io.fcmchannel.sdk.core.network.RestServices;
 import io.fcmchannel.sdk.services.FcmClientRegistrationIntentService;
 
